91b61405eac31db724529b2737b18d6965b27081
Roberto Guerra <uris77@gmail.com>
2012-09-06 08:23:13 -0600

    Refactored tire_pressure_monitoring_tests.py
    Added variables to describe high, low and normal pressures.
    Made the assertions more readable by wrapping them in a method that is
    callable from any test that needs it.

--- TDDMicroExercises.YoursSolutions/uris77/TirePressureMonitoringSystem/tire_pressure_monitoring_tests.py
+++ TDDMicroExercises.YoursSolutions/uris77/TirePressureMonitoringSystem/tire_pressure_monitoring_tests.py
@@ -11,28 +11,37 @@ class AlarmTests(unittest.TestCase):
     def setUp(self):
         self.sensor = Mock()
         self.alarm = Alarm(self.sensor)
+        self.HIGH_PRESSURE = 100
+        self.LOW_PRESSURE = 10
+        self.NORMAL_PRESSURE = 19
 
     def test_turn_alarm_on_when_sensor_is_over_high_threshold(self):
-        self.sensor.pressure_psi_value.return_value = 100
+        self.sensor.pressure_psi_value.return_value = self.HIGH_PRESSURE
         self.alarm.check()
         self.sensor.pressure_psi_value.assert_called_once_with()
-        eq_(True, self.alarm.is_alarm_on())
+        self.alarm_should_be_on()
 
     def test_alarm_should_be_off_when_sensor_is_above_low_threshold_but_below_high_threshold(self):
-        self.sensor.pressure_psi_value.return_value = 18
+        self.sensor.pressure_psi_value.return_value = self.NORMAL_PRESSURE
         self.alarm.check()
         self.sensor.pressure_psi_value.assert_called_once_with()
-        eq_(False, self.alarm.is_alarm_on())
+        self.alarm_should_be_off()
 
     def test_turn_alarm_on_when_sensor_is_below_low_threshold(self):
-        self.sensor.pressure_psi_value.return_value = 10
+        self.sensor.pressure_psi_value.return_value = self.LOW_PRESSURE
         self.alarm.check()
         self.sensor.pressure_psi_value.assert_called_once_with()
-        eq_(True, self.alarm.is_alarm_on())
+        self.alarm_should_be_on()
 
     def test_alarm_should_stay_on_after_pressure_returns_to_normal(self):
-        self.sensor.pressure_psi_value.return_value = 10
+        self.sensor.pressure_psi_value.return_value = self.LOW_PRESSURE
         self.alarm.check()
-        self.sensor.pressure_psi_value.return_value = 19
+        self.sensor.pressure_psi_value.return_value = self.NORMAL_PRESSURE
         self.alarm.check()
+        self.alarm_should_be_on()
+
+    def alarm_should_be_on(self):
         eq_(True, self.alarm.is_alarm_on())
+
+    def alarm_should_be_off(self):
+        eq_(False, self.alarm.is_alarm_on())
